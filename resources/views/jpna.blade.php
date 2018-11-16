0b6fb62ab1e63f32e26a1688de2a1e10825438b6
Rashad-Akhtar <rashadakhtar6@gmail.com>
2018-11-16 22:38:56 +0600

    jpna uploaded

---
+++ resources/views/jpna.blade.php
@@ -0,0 +1,349 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Journal of Public and Nonprofit Affairs</title>
+<meta name="description" content="An open-access journal addressing issues of public administration, nonprofit management, and other issues related to public affairs and management." />
+<meta name="keywords" content="public administration, budgeting, finance, nonprofit, nonprofit management, social entrepreneurship, human resources, human resource management, personnel, governance, community development, economic development, leadership" />
+<meta name="generator" content="Open Journal Systems 2.4.8.2" />
+
+<link rel="icon" href="http://www.jpna.org/public/journals/1/journalFavicon_en_US.png" type="cannot open `/tmp/phpIKaPae&#039; (No such file or directory)" />	<link rel="stylesheet" href="http://www.jpna.org/lib/pkp/styles/pkp.css" type="text/css" />
+<link rel="stylesheet" href="{{ asset('css/jpna/lib/pkp/styles/common.css') }}" type="text/css" />
+<link rel="stylesheet" href="{{ asset('css/jpna/styles/common.css') }}" type="text/css" />
+<link rel="stylesheet" href="{{ asset('css/jpna/styles/compiled.css') }}" type="text/css" />
+
+<!-- Base Jquery -->
+<script type="text/javascript" src="//www.google.com/jsapi"></script>
+    <script type="text/javascript">
+        <!--
+        // Provide a local fallback if the CDN cannot be reached
+        if (typeof google == 'undefined') {
+            document.write(unescape("%3Cscript src='http://www.jpna.org/lib/pkp/js/lib/jquery/jquery.min.js' type='text/javascript'%3E%3C/script%3E"));
+            document.write(unescape("%3Cscript src='http://www.jpna.org/lib/pkp/js/lib/jquery/plugins/jqueryUi.min.js' type='text/javascript'%3E%3C/script%3E"));
+        } else {
+            google.load("jquery", "1.4.4");
+            google.load("jqueryui", "1.8.6");
+        }
+        // -->
+    </script>
+
+
+
+<link rel="stylesheet" href="{{ asset('css/jpna/styles/sidebar.css') }}" type="text/css" />	<link rel="stylesheet" href="http://www.jpna.org/styles/leftSidebar.css" type="text/css" />	<link rel="stylesheet" href="http://www.jpna.org/styles/rightSidebar.css" type="text/css" />	<link rel="stylesheet" href="http://www.jpna.org/styles/bothSidebars.css" type="text/css" />
+<link rel="stylesheet" href="{{ asset('plugins/themes/uncommon/uncommon.css') }}" type="text/css" />
+<link rel="stylesheet" href="{{ asset('plugins/generic/pln/css/pln.css') }}" type="text/css" />
+<link rel="stylesheet" href="{{ asset('plugins/generic/fileValidation/css/ezmodal.min.css') }}" type="text/css" />
+
+<!-- Default global locale keys for JavaScript -->
+
+<script type="text/javascript">
+jQuery.pkp = jQuery.pkp || { };
+jQuery.pkp.locale = { };
+        
+            jQuery.pkp.locale.form_dataHasChanged = 'The data on this form has changed. Continue anyway?';
+</script>
+<!-- Compiled scripts -->
+        
+<script type="text/javascript" src="{{ asset('js/jpna/lib/jquery/plugins/jquery.tag-it.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/jquery/plugins/jquery.cookie.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/functions/fontController.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/functions/general.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/functions/jqueryValidatorI18n.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/classes/Helper.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/classes/ObjectProxy.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/classes/Handler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/classes/linkAction/LinkActionRequest.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/classes/features/Feature.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/SiteHandler.js') }}"></script><!-- Included only for namespace definition -->
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/UrlInDivHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/AutocompleteHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/ExtrasOnDemandHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/form/FormHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/form/AjaxFormHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/form/ClientFormHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/grid/GridHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/controllers/linkAction/LinkActionHandler.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/pages/search/SearchFormHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jpna/lib/pkp/js/statistics/ReportGeneratorFormHandler.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/generic/lucene/js/LuceneAutocompleteHandler.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/jpna/lib/jquery/plugins/jquery.pkp.js') }}"></script>	
+<!-- Form validation -->
+<script type="text/javascript" src="{{ asset('js/jpna/lib/jquery/plugins/validate/jquery.validate.js') }}"></script>
+<script type="text/javascript">
+    <!--
+    // initialise plugins
+    
+    $(function(){
+        jqueryValidatorI18n("http://www.jpna.org", "en_US"); // include the appropriate validation localization
+        
+        $(".tagit").live('click', function() {
+            $(this).find('input').focus();
+        });
+    });
+    // -->
+    
+</script>
+
+    
+<link rel="alternate" type="application/atom+xml" title="Journal of Public and Nonprofit Affairs (atom+xml)" href="http://www.jpna.org/index.php/jpna/gateway/plugin/WebFeedGatewayPlugin/atom" />
+<link rel="alternate" type="application/rdf+xml" title="Journal of Public and Nonprofit Affairs (rdf+xml)" href="http://www.jpna.org/index.php/jpna/gateway/plugin/WebFeedGatewayPlugin/rss" />
+<link rel="alternate" type="application/rss+xml" title="Journal of Public and Nonprofit Affairs (rss+xml)" href="http://www.jpna.org/index.php/jpna/gateway/plugin/WebFeedGatewayPlugin/rss2" />
+<script type="text/javascript">var jArray= ["doc","docx","pdf"];jQuery.fn.extend({ live: function (event, callback) {if (this.selector) {jQuery(document).on(event, this.selector, callback);      } }});</script>
+<script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
+<script type="text/javascript" src="http://www.jpna.org/plugins/generic/fileValidation/js/ezmodal.min.js"></script>
+
+</head>
+<body id="pkp-common-openJournalSystems">
+<div id="container">
+
+<div id="header">
+<div id="headerTitle">
+<h1>
+<img src="http://www.jpna.org/public/journals/1/homeHeaderTitleImage_en_US.jpg" width="1100" height="150" alt="JPNA: Journal of Public and Nonprofit Affairs" />
+</h1>
+</div>
+</div>
+
+<div id="body">
+
+<div id="sidebar">
+                <div id="leftSidebar">
+            <div class="block custom" id="customblock-Editors">
+<p><span class="blockTitle">Editors</span><span style="text-align: center;"></span></p>
+<p><strong>&nbsp;&raquo;&nbsp;Editors-in-Chief:</strong></p>
+<p>&nbsp; Bruce D. McDonald, III<a title="1" href="/index.php/SJRF/pages/view/Mauricio" target="_self"><br /></a>&nbsp; North Carolina State University</p>
+<p>&nbsp;&nbsp;Lindsey M. McDougle<a title="1" href="/index.php/SJRF/pages/view/Mauricio" target="_self"><br /></a>&nbsp; Rutgers University - Newark</p>
+</div>	<div class="block custom" id="customblock-Submit">
+<p><span class="blockTitle">Submission</span></p>
+<p style="text-align: center;"><a href="/index.php/jpna/author/submit/1"><img src="images/submit.png" alt="" /></a></p>
+</div>	<div class="block" id="sidebarUser">
+        <span class="blockTitle">User</span>
+
+                                            <form method="post" action="http://www.jpna.org/index.php/jpna/login/signIn">
+                <table>
+                    <tr>
+                        <td><label for="sidebar-username">Username</label></td>
+                        <td><input type="text" id="sidebar-username" name="username" value="" size="12" maxlength="32" class="textField" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="sidebar-password">Password</label></td>
+                        <td><input type="password" id="sidebar-password" name="password" value="" size="12" class="textField" /></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="checkbox" id="remember" name="remember" value="1" /> <label for="remember">Remember me</label></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="submit" value="Login" class="button" /></td>
+                    </tr>
+                </table>
+            </form>
+                    </div><div class="block" id="sidebarNavigation">
+<span class="blockTitle">Journal Content</span>
+
+
+
+<form id="simplesearchForm" action="http://www.jpna.org/index.php/jpna">
+            <table id="simpleSearchInput">
+        <tr>
+            <td>
+                                                <label for="simpleQuery">Search <br />
+                <input type="text" id="simpleQuery" name="simpleQuery" size="15" maxlength="255" value="" class="textField" /></label>
+                            </td>
+        </tr>
+        <tr>
+            <td><label for="searchField">
+            Search Scope
+            <br />
+            <select id="searchField" name="searchField" size="1" class="selectMenu">
+                <option label="All" value="query">All</option>
+<option label="Authors" value="authors">Authors</option>
+<option label="Title" value="title">Title</option>
+<option label="Abstract" value="abstract">Abstract</option>
+<option label="Index terms" value="indexTerms">Index terms</option>
+<option label="Full Text" value="galleyFullText">Full Text</option>
+
+            </select></label>
+            </td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Search" class="button" /></td>
+        </tr>
+    </table>
+</form>
+
+<br />
+
+    <span class="blockSubtitle">Browse</span>
+<ul>
+    <li><a href="http://www.jpna.org/index.php/jpna/issue/archive">By Issue</a></li>
+    <li><a href="http://www.jpna.org/index.php/jpna/search/authors">By Author</a></li>
+    <li><a href="http://www.jpna.org/index.php/jpna/search/titles">By Title</a></li>
+    
+        </ul>
+</div>
+<div class="block" id="sidebarInformation">
+<span class="blockTitle">Information</span>
+<ul>
+    <li><a href="http://www.jpna.org/index.php/jpna/information/readers">For Readers</a></li>		<li><a href="http://www.jpna.org/index.php/jpna/information/authors">For Authors</a></li>		<li><a href="http://www.jpna.org/index.php/jpna/information/librarians">For Librarians</a></li>	</ul>
+</div>
+
+        </div>
+                        <div id="rightSidebar">
+            <div class="block custom" id="customblock-Social">
+<p><span class="blockTitle">Follow Us</span></p>
+<p style="text-align: center;"><a href="https://www.facebook.com/JPNAffairs/"><img src="images/facebook.png" alt="" /></a>&nbsp;<a href="https://twitter.com/JPNAffairs"><img src="images/twitter.png" alt="" /></a></p>
+</div>	<div class="block custom" id="customblock-COPE">
+<p><span class="blockTitle">Publication Ethics</span><span style="text-align: center;"></span></p>
+<p><a href="https://publicationethics.org/members/journal-public-and-nonprofit-affairs" target="_blank"><img style="margin-right: auto; margin-left: auto; display: block;" src="images/ethics.png" alt="" /></a></p>
+</div>	<div class="block custom" id="customblock-Issue">
+<p><span class="blockTitle">Current Issue</span></p>
+<p><a title="Current Issue" href="/index.php/jpna/issue/view/10"><img style="margin-right: auto; margin-left: auto; display: block;" title="1" src="images/issue.jpg" alt="1" width="140" height="180" align="center" hspace="10" vspace="0" /></a></p>
+</div>	<div class="block custom" id="customblock-Rotator">
+<p><span class="blockTitle">Related Publications</span><span style="text-align: center;"></span></p>
+<script type="text/javascript" language="JavaScript1.1">// <![CDATA[
+/*
+JavaScript Image slideshow:
+By JavaScript Kit (www.javascriptkit.com)
+Over 200+ free JavaScript here!
+*/
+
+var slideimages=new Array()
+var slidelinks=new Array()
+function slideshowimages(){
+for (i=0;i<slideshowimages.arguments.length;i++){
+slideimages[i]=new Image()
+slideimages[i].src=slideshowimages.arguments[i]
+}
+}
+
+function slideshowlinks(){
+for (i=0;i<slideshowlinks.arguments.length;i++)
+slidelinks[i]=slideshowlinks.arguments[i]
+}
+
+function gotoshow(){
+if (!window.winslide||winslide.closed)
+winslide=window.open(slidelinks[whichlink])
+else
+winslide.location=slidelinks[whichlink]
+winslide.focus()
+}
+// ]]></script>
+<p style="text-align: center;"><a href="javascript:gotoshow()"><img src="images/publications.jpg" alt="" name="slide" width="130" height="176" border="0" /></a></p>
+<script type="text/javascript">// <![CDATA[
+//configure the paths of the images, plus corresponding target links
+slideshowimages("images/publications.jpg","images/pub2.gif","images/pub3.png")
+slideshowlinks("http://arp.sagepub.com/","http://jpart.oxfordjournals.org/","http://onlinelibrary.wiley.com/journal/10.1111/(ISSN)1540-6210")
+
+//configure the speed of the slideshow, in miliseconds
+var slideshowspeed=4000
+
+var whichlink=0
+var whichimage=0
+function slideit(){
+if (!document.images)
+return
+document.images.slide.src=slideimages[whichimage].src
+whichlink=whichimage
+if (whichimage<slideimages.length-1)
+whichimage++
+else
+whichimage=0
+setTimeout("slideit()",slideshowspeed)
+}
+slideit()
+// ]]></script>
+</div>	
+        </div>
+        </div>
+
+<div id="main">
+<div id="navbar">
+<ul class="menu">
+    <li id="home"><a href="http://www.jpna.org/index.php/jpna/index">Home</a></li>
+    <li id="about"><a href="http://www.jpna.org/index.php/jpna/about">About</a></li>
+
+                <li id="login"><a href="http://www.jpna.org/index.php/jpna/login">Login</a></li>
+                        <li id="register"><a href="http://www.jpna.org/index.php/jpna/user/register">Register</a></li>
+                                            <li id="search"><a href="http://www.jpna.org/index.php/jpna/search">Search</a></li>
+    
+                <li id="current"><a href="http://www.jpna.org/index.php/jpna/issue/current">Current</a></li>
+        <li id="archives"><a href="http://www.jpna.org/index.php/jpna/issue/archive">Archives</a></li>
+    
+            
+
+                            </ul>
+</div>
+
+    @yield('content')
+
+
+
+<div class="fileValidation">
+<button type="button" class="ezmodal_target" ezmodal-target="#fileValidationBtn" style="display:none">Click here!</button>
+<div id="fileValidationBtn" class="ezmodal">
+    <div class="ezmodal-container">
+        <div class="ezmodal-header">
+            <div class="ezmodal-close" data-dismiss="ezmodal">x</div>
+            Message
+        </div>
+        <div class="ezmodal-content">
+            <h2 style="color:#ef0000;font-family: inherit;"></h2>
+        </div>
+        <div class="ezmodal-footer">
+            <button type="button" data-dismiss="ezmodal">Close</button>       			
+        </div>
+    </div>
+</div>
+</div>	
+<script type="text/javascript">
+
+$(document).ready(function(){
+$("input[name='uploadSubmissionFile']").click( function(event){
+    var error = 0;
+    var msg = '';
+                                              
+    if($('.uploadField')[0].files.length == 0){
+        error = 1;
+        msg = 'submission file field required.';
+    }else{
+        var fileName = $('.uploadField')[0].files[0].name;
+        var fileExt =  fileName.split('.').pop();
+
+        if($.inArray(fileExt, jArray) < 0){
+             error = 2;
+             msg = 'file format not allowed.';
+        }
+    }
+
+    if(error != 0){
+        $('.ezmodal-content h2').text(msg);
+        $('.ezmodal_target').trigger("click");
+        return false;
+    }else{
+        return true;
+    }
+
+});
+
+});
+</script>
+
+<br /><br />
+<div id="pageFooter">ISSN: 2381-3717</div>
+</div><!-- content -->
+</div><!-- main -->
+</div><!-- body -->
+
+
+
+</div><!-- container -->
+</body>
+</html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+@extends('jpna')
+
+@section('content')
+    
+<div>
+    <p>Hello</p>
+</div>
+
+@endsection
